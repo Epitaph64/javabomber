79ab965ef8b8091089b32566cd5eb260d2266671
mazemasterz <mazemasterz@7c47adfe-337c-11de-a81c-fde7d73ceb89>
2009-04-27 22:53:04 +0000

    Private release 5, now as public release 1

---
+++ jBomber/src/jbomber/Player.java
@@ -0,0 +1,181 @@
+package jbomber;
+
+import org.newdawn.slick.Color;
+
+public class Player {
+
+    private boolean human;
+
+    private boolean[] directions;
+
+    private int offsetX, offsetY;
+    private int offsetTileX, offsetTileY;
+
+    private int x,y;
+    private int pid;
+
+    private int direction = 0;
+
+    private Color color;
+
+    private int firepower;
+    private int bombAmt;
+
+    private boolean alive;
+    private int clock;
+
+    private boolean[] moving;
+
+    Player(int x, int y, int number, Color color, boolean human)
+    {
+        this.x = x;
+        this.y = y;
+        this.firepower = 5;
+        this.bombAmt = 5;
+        this.pid = number;
+        this.alive = true;
+        this.clock = 0;
+        this.color = color;
+        this.human = human;
+    }
+
+    public int getPID()
+    {
+        return pid;
+    }
+
+    public void setFirePower(int firep)
+    {
+        firepower = firep;
+    }
+
+    public int getFirePower()
+    {
+        return firepower;
+    }
+    
+    public int getX()
+    {
+        return x;
+    }
+
+    public void setX(int i)
+    {
+        x = i;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    public void setY(int i)
+    {
+        y = i;
+    }
+
+    public void setAlive(boolean aliveValue)
+    {
+        alive = aliveValue;
+    }
+
+    public boolean getAlive()
+    {
+        return alive;
+    }
+
+    public void setClock(int clockTime)
+    {
+        clock = clockTime;
+    }
+
+    public int getClock()
+    {
+        return clock;
+    }
+
+    public void setBombAmt(int amount)
+    {
+        bombAmt = amount;
+    }
+
+    public int getBombAmt()
+    {
+        return bombAmt;
+    }
+
+    public void setDirection(int direction)
+    {
+        this.direction = direction;
+    }
+
+    public int getDirection()
+    {
+        return direction;
+    }
+
+    public Color getColor()
+    {
+        return color;
+    }
+
+    public int getOffSetX()
+    {
+        return offsetX;
+    }
+
+    public int getOffSetY()
+    {
+        return offsetY;
+    }
+
+    public void setOffSetX(int x)
+    {
+        offsetX = x;
+    }
+
+    public void setOffSetY(int y)
+    {
+        offsetY = y;
+    }
+
+    public void setOffSetTileX(int x)
+    {
+        offsetTileX = x;
+    }
+
+    public void setOffSetTileY(int y)
+    {
+        offsetTileY = y;
+    }
+
+    public int getOffSetTileX()
+    {
+        return offsetTileX;
+    }
+
+    public int getOffSetTileY()
+    {
+        return offsetTileY;
+    }
+
+    public boolean getHuman()
+    {
+        return human;
+    }
+
+    public void setHuman(boolean mortal)
+    {
+        human = mortal;
+    }
+
+    public void setDirections(boolean direct[])
+    {
+        directions = direct;
+    }
+
+    public boolean[] getDirections()
+    {
+        return directions;
+    }
+}
