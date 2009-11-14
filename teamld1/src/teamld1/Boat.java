d8ff9dc522c246a705927c9e4efdcb917a9ef3ed
mazemasterz <mazemasterz@7c47adfe-337c-11de-a81c-fde7d73ceb89>
2009-11-14 00:49:58 +0000

    initial commit

---
+++ teamld1/src/teamld1/Boat.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package teamld1;
+
+/**
+ *
+ * @author Epitaph64
+ */
+public class Boat {
+
+    int x, y;
+
+    Boat(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+}
---
+++ teamld1/src/teamld1/Game.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package teamld1;
+
+/**
+ *
+ * @author Epitaph64
+ */
+public interface Game {
+
+    enum gameState {Map, Game};
+
+}
